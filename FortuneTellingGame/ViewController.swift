e769184da6768565b283a63fb0f32d9bab55192d
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-08 17:46:35 +0900

    レイアウト完成

--- FortuneTellingGame/ViewController.swift
+++ FortuneTellingGame/ViewController.swift
@@ -10,32 +10,4 @@ import UIKit
 
 
 
-class ViewController: UIViewController{
-    
-    
-    let nowDate = NSDate()
-    let dateFormat = DateFormatter()
-    let inputDatePicker = UIDatePicker()
-
-    
-    @IBOutlet weak var numberLabel: UILabel!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //日付フィールドの設定
-       
-    }
-    
-    
-    @IBAction func sliderNumber(_ sender: UISlider) {
-        
-        let slidernumber : Int = Int(sender.value)
-        numberLabel.text = String(slidernumber)
-        
-    }
-    
-
-}
 
