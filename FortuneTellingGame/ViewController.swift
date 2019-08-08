fcf7f779a71afbcac4a63aa745ba4e8a9a09ad40
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-08 18:12:34 +0900

    色々ミスった後の機能修正

--- FortuneTellingGame/ViewController.swift
+++ FortuneTellingGame/ViewController.swift
@@ -8,6 +8,49 @@
 
 import UIKit
 
+class ViewController: UIViewController{
+    
+    
+    let nowDate = NSDate()
+    let dateFormat = DateFormatter()
+    let inputDatePicker = UIDatePicker()
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var resultView: UITextView!
+    
+    
+    fileprivate func setPickerNum() -> Int{
+        let date = inputDatePicker.date
+        
+        let year = inputDatePicker.calendar.component(.year, from: date)
+        let month = inputDatePicker.calendar.component(.month, from: date)
+        let day = inputDatePicker.calendar.component(.day, from: date)
+        
+        let num = ((year + month + day) % 4)
+        
+        return num
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+}
+
+    
+    @IBAction func sliderNumber(_ sender: UISlider) {
+        
+        let sliderNumber : Int = Int(sender.value)
+        numberLabel.text = String(sliderNumber)
+
+    }
+    
+    
 
 
-
+}
