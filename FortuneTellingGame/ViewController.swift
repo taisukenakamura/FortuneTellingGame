65d0cfeb8b831efea37a9a0fe6ddf319634ed033
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-08 13:41:55 +0900

    数字を選ぶ機能を付けた

--- FortuneTellingGame/ViewController.swift
+++ FortuneTellingGame/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController{
+    
+    
+    let nowDate = NSDate()
+    let dateFormat = DateFormatter()
+    let inputDatePicker = UIDatePicker()
+
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //日付フィールドの設定
+       
     }
-
+    
+    
+    @IBAction func sliderNumber(_ sender: UISlider) {
+        
+        let slidernumber : Int = Int(sender.value)
+        numberLabel.text = String(slidernumber)
+        
+    }
+    
 
 }
 
