3f2a24308f621a21ba8b7109e6712ce23fc7a768
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-11 20:19:35 +0900

    バグ修正

--- FortuneTellingGame/ViewController.swift
+++ FortuneTellingGame/ViewController.swift
@@ -21,31 +21,6 @@ class ViewController: UIViewController{
     
     @IBOutlet weak var bloodSegment: UISegmentedControl!
     @IBOutlet weak var setDatePicker: UIDatePicker!
-    //    スライダーの番号の初期化
-    var sliderNumber : Int = 50
-    //    DatePickerから選択されたデータを取り出し、変数に代入する
-    fileprivate func setPickerNumber() -> Int {
-        //        一旦全てのデータを格納
-        let date = setDatePicker.date
-        //        calandar.componentで取り出す
-        let year = setDatePicker.calendar.component(.year, from: date)
-        let month = setDatePicker.calendar.component(.month, from: date)
-        let day =  setDatePicker.calendar.component(.day, from: date)
-        //        割った余りを定数に格納
-        let num : Int = ((year + month + day) % 4)
-        //        戻り値として返す
-        return num
-        
-    }
-    //    スライダーを動く関数
-    @IBAction func sliderNumber(_ sender: UISlider) {
-        //     sender.valueで選んだ数字をInt型にしてていい数に格納
-        let sliderNumber : Int = Int(sender.value)
-        //        そのあと文字型にしてラベルに代入する
-        numberLabel.text = String(sliderNumber)
-        
-    }
-    
     
     //   それぞれにデータを並列させる
     let pickerShow : [String] = [
@@ -70,6 +45,33 @@ class ViewController: UIViewController{
         "ふと口をついて出た曲をダウンロードするか、ラジオ番組にリクエストメールを出しましょう。",
     ]
     
+    //    スライダーの番号の初期化
+    var sliderN : Int = 50
+    //    DatePickerから選択されたデータを取り出し、変数に代入する
+    func setPickerNumber() -> Int {
+        //        一旦全てのデータを格納
+        let date = setDatePicker.date
+        //        calandar.componentで取り出す
+        let year = setDatePicker.calendar.component(.year, from: date)
+        let month = setDatePicker.calendar.component(.month, from: date)
+        let day =  setDatePicker.calendar.component(.day, from: date)
+        //        割った余りを定数に格納
+        let num : Int = ((year + month + day) % 4)
+        //        戻り値として返す
+        return num
+       
+    }
+    //   スライダーを動く関数
+    @IBAction func sliderNumber(_ sender: UISlider) {
+        
+        sliderN = Int(sender.value)
+        // 文字型にしてラベルに代入する,整数で受け取る
+        numberLabel.text = String(sliderN)
+        
+    }
+    
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -77,36 +79,38 @@ class ViewController: UIViewController{
     }
     
     
-    //    データの個数と余り値の個数が血液型の種類数が同一なため、同じ定数に代入して、戻り値として返す。
-    //    以下の二つの関数はInt型の引数として使う
+    // データの個数と余り値の個数が血液型の種類数が同一なため、同じ定数に代入して、戻り値として返す。
+    // 以下の二つの関数はInt型の引数として使う
     
-    func setBloodNumber() -> Int{
+    func setBloodNumber() -> Int {
         
         let num = bloodSegment.selectedSegmentIndex
+        
         return num
         
     }
     
-    func setSliderNumber() -> Int{
+    func setSliderNumber() -> Int {
         
-        let num = (sliderNumber % 4)
+        
+        let num = (sliderN % 4)
         
         return num
     }
-    //    結果を表すtextViewに結果を代入できるような関数を作って、String型の戻り値として返す
-    func settingTextView(setPickerNumber : Int,setBloodNumber : Int,setSliderNumber : Int) -> String{
-        return pickerShow[setPickerNumber] + bloodType[setBloodNumber] + sliderMessage[setSliderNumber]
-    }
-    
+    // 結果を表すtextViewに結果を代入できるような関数を作って、String型の戻り値として返す
+//    func settingTextView(PickerN : Int, BloodN : Int, SliderN : Int) -> String {
+//        return pickerShow[PickerN] + bloodType[BloodN] + sliderMessage[SliderN]
+//    }
+//
     
     
     
     
     
     @IBAction func showButton(_ sender: Any) {
-        //        関数を引数として渡して、textVuewに代入する
-        resultView.text = settingTextView(setPickerNumber: setPickerNumber(), setBloodNumber: setBloodNumber(), setSliderNumber: setSliderNumber())
-        
+        // 関数を引数として渡して、textVuewに代入する
+//        resultView.text = settingTextView(PickerN: setPickerNumber(), BloodN: setBloodNumber(), SliderN: setSliderNumber())
+       resultView.text = pickerShow[setPickerNumber()] + bloodType[setBloodNumber()] + sliderMessage[setSliderNumber()]
     }
 }
 
