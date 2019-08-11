84ac38f46e7b5329e7005f6215a9e2fb1f79a04d
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-11 22:36:29 +0900

    コメント追加

--- FortuneTellingGame/ViewController.swift
+++ FortuneTellingGame/ViewController.swift
@@ -10,19 +10,14 @@ import UIKit
 
 class ViewController: UIViewController{
     
-    
-    //    let nowDate = NSDate()
-    //    let dateFormat = DateFormatter()
-    //    let inputDatePicker = UIDatePicker()
-    
-    
+    //それぞれ変数として扱えるように定義しておく
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var resultView: UITextView!
     
     @IBOutlet weak var bloodSegment: UISegmentedControl!
     @IBOutlet weak var setDatePicker: UIDatePicker!
     
-    //   それぞれにデータを並列させる
+    // それぞれにデータを並列させる
     let pickerShow : [String] = [
         "あなたは山に愛されています。\n",
         "あなたは海に愛されています。\n",
@@ -45,25 +40,25 @@ class ViewController: UIViewController{
         "ふと口をついて出た曲をダウンロードするか、ラジオ番組にリクエストメールを出しましょう。",
     ]
     
-    //    スライダーの番号の初期化
+    // スライダーの番号の初期化
     var sliderN : Int = 50
-    //    DatePickerから選択されたデータを取り出し、変数に代入する
+    // DatePickerから選択されたデータを取り出し、変数に代入する
     func setPickerNumber() -> Int {
-        //        一旦全てのデータを格納
+        // 一旦全てのデータを格納
         let date = setDatePicker.date
-        //        calandar.componentで取り出す
+        // calandar.componentで取り出す
         let year = setDatePicker.calendar.component(.year, from: date)
         let month = setDatePicker.calendar.component(.month, from: date)
         let day =  setDatePicker.calendar.component(.day, from: date)
-        //        割った余りを定数に格納
+        // 割った余りを定数に格納
         let num : Int = ((year + month + day) % 4)
-        //        戻り値として返す
+        // 戻り値として返す
         return num
        
     }
-    //   スライダーを動く関数
+    // スライダーを動く関数
     @IBAction func sliderNumber(_ sender: UISlider) {
-        
+        //スライダーで送られてくる値を整数として受け取る
         sliderN = Int(sender.value)
         // 文字型にしてラベルに代入する,整数で受け取る
         numberLabel.text = String(sliderN)
@@ -74,42 +69,33 @@ class ViewController: UIViewController{
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
         
     }
     
     
     // データの個数と余り値の個数が血液型の種類数が同一なため、同じ定数に代入して、戻り値として返す。
-    // 以下の二つの関数はInt型の引数として使う
+    // 以下の二つの関数はInt型の戻り値を持つ関数として使う
     
     func setBloodNumber() -> Int {
-        
+        // セグメントで選ばれたものを数値として受け取る
         let num = bloodSegment.selectedSegmentIndex
-        
+        // 戻り値
         return num
         
     }
     
     func setSliderNumber() -> Int {
         
-        
+        // スライダーで選ばれた数字を4で割って、余りの0,1,2,3を代入する
         let num = (sliderN % 4)
-        
+        // 戻り値
         return num
     }
-    // 結果を表すtextViewに結果を代入できるような関数を作って、String型の戻り値として返す
-//    func settingTextView(PickerN : Int, BloodN : Int, SliderN : Int) -> String {
-//        return pickerShow[PickerN] + bloodType[BloodN] + sliderMessage[SliderN]
-//    }
-//
-    
-    
-    
-    
-    
+
+    // ボタンを押した時の挙動
     @IBAction func showButton(_ sender: Any) {
-        // 関数を引数として渡して、textVuewに代入する
-//        resultView.text = settingTextView(PickerN: setPickerNumber(), BloodN: setBloodNumber(), SliderN: setSliderNumber())
+        // 関数を引数として渡して、textViewに代入する
        resultView.text = pickerShow[setPickerNumber()] + bloodType[setBloodNumber()] + sliderMessage[setSliderNumber()]
     }
 }
